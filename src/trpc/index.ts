6a236613b1433d889afb6592a815cae7ae1e4417
rohit saini <r7665415553@gmail.com>
2023-10-25 00:07:00 +0530

    update Dashboard and add Files schema

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { router ,publicProcedure} from './trpc';
+import { router ,publicProcedure,privateProcedure} from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
 
@@ -26,6 +26,14 @@ export const appRouter = router({
     }
 
     return {success: true}
+  }),
+  getUserFiles: privateProcedure.query(async ({ctx}) => {
+    const {userId,user} = ctx
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    })
   })
 });
 
